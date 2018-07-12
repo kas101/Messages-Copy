38d566c22db740c595be56a1ff5fda1be5b26002
kas101 <kasjacob@outlook.com>
2018-07-12 10:10:37 +0200

    initial commit

---
+++ MessaJes/app/src/main/java/com/kasjacoboutlook/messajes/LaucherScreen.java
@@ -0,0 +1,13 @@
+package com.kasjacoboutlook.messajes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaucherScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laucher_screen);
+    }
+}
